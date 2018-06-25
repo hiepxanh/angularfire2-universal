bbf710fb87de366237befe7a31478497a6a99931
hiepxanh <hiepxanh@gmail.com>
2018-06-25 09:52:20 +0700

    Update index.ts

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,1 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCoN2-lrBnr69tTj99GPTfLj7xXj2uxOgQ",
-    authDomain: "routing-aadd4.firebaseapp.com",
-    databaseURL: "https://routing-aadd4.firebaseio.com",
-    projectId: "routing-aadd4",
-    storageBucket: "routing-aadd4.appspot.com",
-    messagingSenderId: "1075040081520"
-}
+
